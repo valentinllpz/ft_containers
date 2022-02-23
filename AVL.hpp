6c73da4921a830fdeca1e078149c74f95c332948
Valentin LUGAND LOPEZ <vlugand-@e1r9p9.42.fr>
2022-02-23 18:03:41 +0100

    Working on AVL - insert seems broken

--- AVL.hpp
+++ AVL.hpp
@@ -6,7 +6,7 @@
 /*   By: vlugand- <vlugand-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 14:59:27 by vlugand-          #+#    #+#             */
-/*   Updated: 2022/02/22 17:00:38 by vlugand-         ###   ########.fr       */
+/*   Updated: 2022/02/23 18:01:27 by vlugand-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,51 +30,52 @@ class AVLTree
 {
 	public:
 
-		Node	*root;
+		Node<T>	*root;
 	
 		AVLTree() : root(NULL) {}
 
 		bool	isEmpty() { return (root == NULL ? true : false); }
 
-		int		height(Node *n)  // Get Tree's height from Node *n recursively
+		int		height(Node<T> *n)  // Get the tree's height from Node *n recursively
 		{
 			if (n == NULL)
 				return (-1);
 			int	lh = height(n->left);
 			int	rh = height(n->right);
-			return (lh > rh ? lh + 1 : rh + 1)
+			return (lh > rh ? lh + 1 : rh + 1);
 		}
 
-		int		balance(Node *n) // Get Balance factor of Node *n
+		int		balance(Node<T> *n) // Get the balance factor of Node *n
 		{
 			if (n == NULL)
 				return (-1);
 			return (height(n->left) - height(n->right));
 		}
 
-		Node	*rightRotate(Node *y) // Performs a right rotation if the tree is left heavy
+		Node<T>	*rightRotate(Node<T> *y)
 		{
-			Node *x = y->left;
-			node *subtree = x->right;
+			Node<T> *x = y->left;
+			Node<T> *subtree = x->right;
 			x->right = y;
 			y->left = subtree;
 			return (x);
 		}
 
-		Node	*leftRotate(Node *x)  // Performs a right rotation if the tree is right heavy
+		Node<T>	*leftRotate(Node<T> *x)
 		{
-			Node *y = x->right;
-			node *subtree = y->left;
+			Node<T> *y = x->right;
+			Node<T> *subtree = y->left;
 			y->left = x;
 			x->right = subtree;
 			return (y);
 		}
 		
-		Node	*insert(Node *n, Node *new_node)
+		Node<T>	*insert(Node<T> *n, Node<T> *new_node)
 		{
 			if (n == NULL)
 			{
 				n = new_node;
+				std::cout << "Value inserted successfully: " << n->value << " | root = " << root->value << std::endl;
 				return (n);
 			}
 			if (new_node->value < n->value)
@@ -83,7 +84,98 @@ class AVLTree
 				n->right = insert(n->right, new_node);
 			else // no duplicate allowed
 				return (n);
-			
+			int	bf = balance(n);
+			if (bf > 1 && new_node->value < n->left->value)  // if the tree is left heavy and our new_node->value was added after n->left
+				return (rightRotate(n));
+			if (bf < -1 && new_node->value > n->right->value) // if the tree is right heavy and our new->node value was added after n->right
+				return (leftRotate(n));
+			if (bf > 1 && new_node->value > n->left->value) // if the tree is left heavy and our new_node->value was added to n->right
+			{
+				n->left = leftRotate(n);
+				return (rightRotate(n));
+			}
+			if (bf < -1 && new_node->value < n->right->value) // if the tree is right heavy and our new_node->value was added to n->left
+			{
+				n->right = rightRotate(n);
+				return (leftRotate(n));
+			}
+			return (n);
 		}
 
-}
+		Node<T>	*searchNode(Node<T> *n, T value) // call with n = root the first time to search all the tree
+		{
+			if (n == NULL)
+				return (NULL);
+			if (value < n->value)
+				return (searchNode(n->left, value));
+			else if (value > n->value)
+				return (searchNode(n->right, value));
+			else // value == n->value
+				return (n);
+		}
+
+		Node<T>	*deleteNode(Node<T> *n, T value)
+		{
+			if (n == NULL)
+				return (NULL);
+			if (value < n->value)
+				n->left = deleteNode(n->left, value);
+			else if (value > n->value)
+				n->right = deleteNode(n->right, value);
+			else
+			{
+				if (n->left == NULL) // node with only one child or no child
+				{
+					Node<T> *tmp = n->right;
+					delete n;
+					return (tmp);
+				}
+				else if (n->right == NULL) // node with only one child or no child
+				{
+					Node<T> *tmp = n->left;
+					delete n;
+					return (tmp);
+				}
+				else // node with 2 children
+				{
+					Node<T> *tmp = n->right; // Here we are going down to the smallest value of the n right descendants (= smallest value bigger than n->value)
+					while (tmp->left)
+						tmp = tmp->left;
+					n->value = tmp->value; // we replace the value in node n
+					n->right = deleteNode(n->right, n->value); // from the right child of n, we now delete the duplicate value
+				}
+
+				int bf = getBalanceFactor(n);
+				if (bf == 2 && getBalanceFactor(n->left) >= 0) // Left Left Imbalance
+					return rightRotate(n);
+				else if (bf == 2 && getBalanceFactor(n->left) == -1) // Left Right Imbalance
+				{
+					n->left = leftRotate(n->left);
+					return rightRotate(n);
+				}
+				else if (bf == -2 && getBalanceFactor(n->right) <= 0) // Right Right Imbalance	
+					return leftRotate(n);
+				else if (bf == -2 && getBalanceFactor(n->right) == 1) // Right Left Imbalance
+				{
+					n->right = rightRotate(n->right);
+					return leftRotate(n);
+    			}
+			}
+			return (n);
+		}
+
+		// DEBUG
+		void	print(Node<T> * r, int space)
+		{
+			if (r == NULL)
+				return ;
+			space += 5; 
+			print(r -> right, space);
+			std::cout << std::endl;
+			for (int i = 5; i < space; i++)
+				std::cout << " ";
+			std::cout << r -> value << "\n"; 
+			print(r -> left, space); 
+		}
+		
+};
--- test.cpp
+++ test.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: valentin <valentin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vlugand- <vlugand-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 14:32:00 by vlugand-          #+#    #+#             */
-/*   Updated: 2022/02/16 12:41:59 by valentin         ###   ########.fr       */
+/*   Updated: 2022/02/23 17:56:55 by vlugand-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,23 @@
 #include <stdexcept>
 #include "vector.hpp"
 #include <list>
+#include "AVL.hpp"
 
 #define TESTED_NAMESPACE ft
 #define TESTED_TYPE int
 
 int main ()
 {
-  ft::vector<int> myvector;
-  for (int i=1; i<5; i++) myvector.push_back(i);
+	AVLTree<int> tree;
+	Node<int> * new_node = new Node<int>();
 
-  typedef ft::vector<int>::iterator iter_type;
-                                                         // ? 9 8 7 6 5 4 3 2 1 0 ?
-  iter_type from (myvector.begin());                     //   ^
-                                                         //         ------>
-  iter_type until (myvector.end());                      //                       ^
-                                                         //
-  ft::vector_reverse_iterator<iter_type> rev_until (from);     // ^
-                                                         //         <------
-  ft::vector_reverse_iterator<iter_type> rev_from (until);     //                     ^
-
-  std::cout << "myvector:";
-//   while (rev_from != rev_until)
-//   {
-//     std::cout << *rev_from << " " << *rev_until;
-//   	std::cout << '\n';
-// 	  *rev_from++;
-//   }
-
-	std::cout << *rev_until << std::endl;
-  return 0;
+	for (int i = 0; i < 15; i++)
+	{
+		new_node->value = i;
+		tree.insert(tree.root, new_node);
+	}
+	tree.print(tree.root, 5);
+	return 0;
 }
 
 
